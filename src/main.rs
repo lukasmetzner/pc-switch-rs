8f2ff780f05c37d5b27cb73bd608d08435869187
lukasmetzner <lukas@metzner.io>
2024-01-31 19:26:42 +0100

    single pin instance with reference

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ use std::io::{BufReader, Read};
 use std::thread;
 use std::time::Duration;
 
-use rppal::gpio::Gpio;
+use rppal::gpio::{Gpio, OutputPin};
 use std::net::{TcpListener, TcpStream};
 
 const CRAP: [char; 3] = ['\n', '\0', '\t'];
 const PIN: u8 = 21;
 
-fn switch_for_ms(ms: u64) -> Result<()> {
-    // TODO: set_low() does not turn off the relay
-    let mut pin = Gpio::new()?.get(PIN)?.into_output();
-    pin.set_high();
+fn switch_for_ms(pin: &mut OutputPin, ms: u64) -> Result<()> {
+    pin.set_low();
     thread::sleep(Duration::from_millis(ms));
+    pin.set_high();
+    thread::sleep(Duration::from_millis(50));
     Ok(())
 }
 
@@ -23,7 +23,7 @@ fn remove_crap(s: &mut String) {
     s.truncate(new_len);
 }
 
-fn handle_client(stream: &mut TcpStream) -> Result<()> {
+fn handle_client(stream: &mut TcpStream, pin: &mut OutputPin) -> Result<()> {
     let mut reader = BufReader::new(stream);
     loop {
         let mut buff: [u8; 8] = [0; 8];
@@ -38,16 +38,17 @@ fn handle_client(stream: &mut TcpStream) -> Result<()> {
                 }
             }
         };
-        switch_for_ms(ms)?;
+        switch_for_ms(pin, ms)?;
     }
     Ok(())
 }
 
 fn main() -> Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8000")?;
+    let mut pin = Gpio::new()?.get(PIN)?.into_output_high();
 
     for stream in listener.incoming() {
-        handle_client(&mut (stream?))?;
+        handle_client(&mut (stream?), &mut pin)?;
     }
 
     Ok(())
